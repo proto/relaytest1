115ef45c02d9aff6fc2b229b17f2bbac034a8cdf
proto <proto@users.noreply.github.com>
2023-07-30 18:28:41 +0200

    working...

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+let maxTemp = 0
 power.fullPowerEvery(15000, function () {
     led.setBrightness(10)
-    basic.showString("" + (input.temperature()))
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(10000)
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    maxTemp = 25
+    if (input.temperature() >= maxTemp) {
+        basic.showString("" + (input.temperature()))
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(10000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
     power.lowPowerRequest()
 })
