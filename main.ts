560b69f52f06d150ed5dd876c80cc14a0079eb7f
proto <proto@users.noreply.github.com>
2023-07-30 18:22:02 +0200

    simple simple...

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 power.fullPowerEvery(15000, function () {
     led.setBrightness(10)
-    if (input.lightLevel() > 100) {
-        if (input.temperature() >= 20) {
-            basic.showString("" + input.temperature() + "-" + input.lightLevel())
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            basic.pause(10000)
-            pins.digitalWritePin(DigitalPin.P0, 0)
-        }
-    }
-    power.lowPowerPause(30000)
+    basic.showString("" + (input.temperature()))
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(10000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    power.lowPowerRequest()
 })
