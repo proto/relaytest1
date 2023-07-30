9e8a269ad500ea4e7cee2461ec657bf4229e4557
proto <proto@users.noreply.github.com>
2023-07-30 17:50:18 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 power.fullPowerEvery(15000, function () {
     led.setBrightness(10)
     if (input.lightLevel() > 100) {
-        if (input.temperature() >= 25) {
+        if (input.temperature() >= 20) {
             basic.showString("" + input.temperature() + "-" + input.lightLevel())
             pins.digitalWritePin(DigitalPin.P0, 1)
-            basic.pause(5000)
+            basic.pause(10000)
             pins.digitalWritePin(DigitalPin.P0, 0)
         }
     }
-    power.lowPowerRequest()
+    power.lowPowerPause(30000)
 })
